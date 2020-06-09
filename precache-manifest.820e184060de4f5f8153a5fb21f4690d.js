c9d91a5357d0538b3c91bcf601109074b5d0b6b4
Ayush <ayushssingh98@gmail.com>
2020-06-09 22:11:55 +0530

    Updates

---
+++ precache-manifest.820e184060de4f5f8153a5fb21f4690d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dd4b9d3e0f06778f46657737e4d0ac",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae912df31bfa245351c6",
+    "url": "/portfolio/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "c99acbd71376adc586f5",
+    "url": "/portfolio/static/js/2.53777436.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.53777436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae912df31bfa245351c6",
+    "url": "/portfolio/static/js/main.2c7c3ca3.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "82494cda253641521a12945d67ae829b",
+    "url": "/portfolio/static/media/profile.82494cda.jpg"
+  }
+]);
